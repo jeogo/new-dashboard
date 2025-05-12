e95f5c9fb10c63cdf288505b05aa7b0c64ec14a0
jeogo <65985256+jeogo@users.noreply.github.com>
2025-05-12 02:07:57 +0100

    Update apiClient.ts

--- src/app/utils/apiClient.ts
+++ src/app/utils/apiClient.ts
@@ -1,7 +1,7 @@
 // src/utils/apiClient.ts
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-jfe3.onrender.com";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-aotc.onrender.com";
 
 type ApiResponse<T = any> = {
   data?: T;
