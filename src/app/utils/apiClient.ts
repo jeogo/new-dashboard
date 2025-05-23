108dd5d499d0afe5abb4d7e7bd37de48a6eb3160
jeogo <65985256+jeogo@users.noreply.github.com>
2025-05-23 19:11:11 +0100

    Update apiClient.ts

--- src/app/utils/apiClient.ts
+++ src/app/utils/apiClient.ts
@@ -1,7 +1,7 @@
 // src/utils/apiClient.ts
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-b8de.onrender.com";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://storebot-msp6.onrender.com";
 
 type ApiResponse<T = any> = {
   data?: T;
